0359554e118f5e8299e1edbe89d1afdfba84da5e
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2024-10-20 02:13:49 +0200

    add contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,72 @@
 import css from './ContactForm.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
 
-const ContactForm = () => {
-  return <div>ContactForm</div>;
+const ContactForm = ({ submit }) => {
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Minimum 3 letters')
+      .max(50, 'Maximum 50 letters')
+      .required('Name is required'),
+    number: Yup.string()
+      .matches(/^[0-9]{3}-[0-9]{2}-[0-9]{2}$/, 'Must be in format 123-45-67')
+      .required('Number is required'),
+    id: Yup.string().required(),
+  });
+
+  const nameId = useId();
+  const numberId = useId();
+
+  const handleSubmit = (values, actions) => {
+    submit(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        number: '',
+        id: '',
+      }}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className={css.form}>
+        <div className={css.field}>
+          <label htmlFor={nameId}>Name</label>
+          <Field
+            className={css.input}
+            type="text"
+            name="name"
+            id={nameId}
+          ></Field>
+          <ErrorMessage
+            className={css.errorMsg}
+            name="name"
+            component="span"
+          ></ErrorMessage>
+        </div>
+
+        <div className={css.field}>
+          <label htmlFor={numberId}>Number</label>
+          <Field
+            className={css.input}
+            type="text"
+            name="number"
+            id={numberId}
+          ></Field>
+          <ErrorMessage
+            className={css.errorMsg}
+            name="number"
+            component="span"
+          ></ErrorMessage>
+        </div>
+        <button className={css.addBtn}>Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
